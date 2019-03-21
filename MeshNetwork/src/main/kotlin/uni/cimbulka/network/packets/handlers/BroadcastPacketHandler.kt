12c2c0de734d94227cb6043c31c4aba42c99b533
Matyas Cimbulka <2cimbm33@solent.ac.uk>
2019-03-21 19:06:41 +0000

    Modifies BroadcastPacketHandler to resend packets once processed.
    Corespondes to version 4.

--- MeshNetwork/src/main/kotlin/uni/cimbulka/network/packets/handlers/BroadcastPacketHandler.kt
+++ MeshNetwork/src/main/kotlin/uni/cimbulka/network/packets/handlers/BroadcastPacketHandler.kt
@@ -14,6 +14,9 @@ internal class BroadcastPacketHandler : PacketHandler<BroadcastPacket> {
             if (data is UpdateData) {
                 processUpdates(data, session)
             }
+
+            // Resend the packet
+            PacketSender.send(packet, session)
         }
     }
 
